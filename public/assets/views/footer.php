457a03977cfdab7fcf1db0614bdcc9495cd1a8ef
AntoineCuinet <antoine.cuinetdu70200@gmail.com>
2024-03-09 22:56:27 +0100

    animation de liens

--- public/assets/views/footer.php
+++ public/assets/views/footer.php
@@ -12,13 +12,13 @@
             <h3>Informations</h3>
             <ul>
                 <li>
-                    <p><span>Lieu: </span>25000 Besançon</p>
+                    <p><span>Lieu: </span><a class="footer-links" href="https://maps.app.goo.gl/iv6a3NZTZPE6r9pk7" target="_blank">25000 Besançon</a></p>
                 </li>
                 <li>
-                    <p><span>Email: </span><a href="mailto:antoine@acuinet.fr?subject=Demande%20de%20création%20de%20site%20web&body=Bonjour%2C%20%0D%0A%0D%0AJe%20vous%20contacte%20pour...">antoine@acuinet.fr</a></p>
+                    <p><span>Email: </span><a class="footer-links" href="mailto:antoine@acuinet.fr?subject=Demande%20de%20création%20de%20site%20web&body=Bonjour%2C%20%0D%0A%0D%0AJe%20vous%20contacte%20pour...">antoine@acuinet.fr</a></p>
                 </li>
                 <li>
-                    <p><span>Téléphone: </span><a href="tel:+33625330842">06 25 33 08 42</a></p>
+                    <p><span>Téléphone: </span><a class="footer-links" href="tel:+33625330842">06 25 33 08 42</a></p>
                 </li>
             </ul>
         </div>
@@ -26,19 +26,19 @@
         <div class="bloc">
             <h3>Liens pratiques</h3>
             <ul>
-                <li><a href="./portfolio.php">Portfolio</a></li>
-                <li><a href="./about.php">À propos</a></li>
-                <!-- <li><a href="./blog.php">Blog</a></li> -->
-                <li><a href="./contact.php">Contact</a></li>
-                <li><a href="./legal.php">Mentions légales</a></li>
+                <li><a class="footer-links" href="./portfolio.php">Portfolio</a></li>
+                <li><a class="footer-links" href="./about.php">À propos</a></li>
+                <!-- <li><a class="footer-links" href="./blog.php">Blog</a></li> -->
+                <li><a class="footer-links" href="./contact.php">Contact</a></li>
+                <li><a class="footer-links" href="./legal.php">Mentions légales</a></li>
             </ul>
         </div>
 
         <div class="bloc">
             <h3>Réseaux</h3>
             <ul>
-                <li><a href="#">LinkedIn</a></li>
-                <li><a href="#">GitHub</a></li>
+                <li><a class="footer-links" href="#">LinkedIn</a></li>
+                <li><a class="footer-links" href="#">GitHub</a></li>
             </ul>
         </div>
     </div>
