1a19568dd5fcbd2d8be4ce7c4e9f1f69434c7038
AntoineCuinet <antoine.cuinetdu70200@gmail.com>
2024-03-09 16:42:35 +0100

    mise en ligne

--- public/assets/views/footer.php
+++ public/assets/views/footer.php
@@ -15,7 +15,7 @@
                     <p><span>Lieu: </span>25000 Besançon</p>
                 </li>
                 <li>
-                    <p><span>Email: </span><a href="mailto:antoine.cuinet70200@gmail.com?subject=Demande%20de%20création%20de%20site%20web&body=Bonjour%2C%20%0D%0A%0D%0AJe%20vous%20contacte%20pour...">antoine.cuinet70200@gmail.com</a></p>
+                    <p><span>Email: </span><a href="mailto:antoine@acuinet.fr?subject=Demande%20de%20création%20de%20site%20web&body=Bonjour%2C%20%0D%0A%0D%0AJe%20vous%20contacte%20pour...">antoine@acuinet.fr</a></p>
                 </li>
                 <li>
                     <p><span>Téléphone: </span><a href="tel:+33625330842">06 25 33 08 42</a></p>
