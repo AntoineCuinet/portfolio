d0219f4b300d6f00e97bc2b55008db9af94eb3d1
AntoineCuinet <antoine.cuinetdu70200@gmail.com>
2024-03-18 23:15:54 +0100

    continue form

--- public/contact.php
+++ public/contact.php
@@ -5,32 +5,38 @@ $description_page = 'Page de Contact';
 
 include('./assets/views/header.php');
 
-echo <<<HTML
-<section class="login-container">
-    <div class="wrapper">
-        <div class="wrapper-contain">
-            <h2 class="login-title">Contact</h2>
-            <form method="POST" action="contact.php" class="form">
-                
-                <div class="form-group">
-                    <input type="email" name="email" value="" required>
-                    <i class='bx bx-envelope' ></i>
-                    <span>T'on meilleur Email</span>
-                </div>
-                <br>
+echo '<section class="login-container">',
+'<div class="wrapper">',
+'<div class="wrapper-contain">',
+'<h2>', $title_page, '</h2>',
+'<form action="./contact.php" method="POST">',
 
-                <div class="form-group">
-                    <input type="password" name="password" value="" required>
-                    <i class='bx bx-lock-alt' ></i>
-                    <span>T'on meilleur mot de passe</span>
-                </div>
-                <br>
+'<div class="form-field">',
+'<input type="text" name="firstname" id="firstname" value="" required>',
+'<label for="firstname">Prénom <span class="form-oblig">*</span></label>',
+'</div>',
+'<div class="form-field">',
+'<input type="text" name="lastname" id="lastname" value="" required>',
+'<label for="lastname">Nom <span class="form-oblig">*</span></label>',
+'</div>',
+'<div class="form-field">',
+'<input type="email" name="email" id="email" value="" required>',
+'<label for="email">E-mail <span class="form-oblig">*</span></label>',
+'</div>',
 
-                <input type="submit" value="Me connecter" name="valider" class="btn btn-login">
-            </form>
-        </div>
-    </div>
-</section>
-HTML;
+'<div class="form-field">',
+'<input type="text" name="subject" id="subject" value="" required>',
+'<label for="subject">Sujet <span class="form-oblig">*</span></label>',
+'</div>',
+'<div class="form-field">',
+'<textarea name="message" id="message" value="" rows="4" maxlength="1000" autocomplete="off" spellcheck="true" required></textarea>',
+'<label for="message">Message <span class="form-oblig">*</span></label>',
+'</div>',
+
+'<input type="submit" value="Me connecter" name="valider" class="btn">',
+'</form>',
+'</div>',
+'</div>',
+'</section>';
 
 include('./assets/views/footer.php');
